7c729d6432f18b72b3beaf711d9ac0dc8e207d6c
kjs36202 <kjs36202@naver.com>
2021-10-24 00:18:16 +0900

    test2

---
+++ test/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int a; 
+        a = sc.nextInt();
+        System.out.print("출력값 : ");
+        System.out.println(a);
+        System.out.println("hi");
+        
+
+
+    }
+}
